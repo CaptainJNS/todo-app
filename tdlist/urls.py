5ac5245a1cc467c5ec680f10ac0cdd26c7f73459
CaptainJNS <Sereda9623@gmail.com>
2019-03-31 00:22:47 +0200

    Add project

---
+++ tdlist/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from todo_list.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+	url(r'^$', index),
+	path('register/', registration),
+	path('login/', login),
+	path('logout/', logout),
+	path('add_list/', add_list),
+	path('change_status/', change_status),
+	path('delete_task/', delete_task),
+	path('delete_list/', delete_list),
+	path('add_task/', add_task),
+	path('add_list/', add_list),
+	path('edit_list/', edit_list),
+	path('edit_task/', edit_task),
+	path('prup/', prup),
+	path('prdown/', prdown),
+    path('admin/', admin.site.urls),
+] + staticfiles_urlpatterns()
+
---
+++ todo_list/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+	name = models.CharField(max_length = 255)
+	done = models.BooleanField(default=False)
+	deadline = models.DateTimeField(null = True, blank = True)
+	priority = models.IntegerField(default = 0)
+	#tdlist = models.ForeignKey(TDList, on_delete=models.CASCADE, null = True, blank = True)
+
+	def __str__(self):
+		return self.name
+
+class TDList(models.Model):
+	name = models.CharField(max_length = 64)
+	task = models.ManyToManyField(Task, blank = True)		
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null = True)	
+
+	def __str__(self):
+		return self.name
